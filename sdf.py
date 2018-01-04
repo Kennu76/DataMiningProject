9283053b658121d6c9413ca07124c30ba6527f27
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2018-01-04 07:50:38 +0200

    about ahrs25

--- sdf.py
+++ sdf.py
@@ -6,14 +6,21 @@ sensor_data = open(sensor_path)
 i = 1
 bots = set()
 
+item = "sda"
 for line in iter(sensor_data):
-    # if i == 10:
-    #     break
     points = json.loads(line)
     # print points[0]
     bots.add(points['meta']['botid'])
+    # print(points["meta"]["secs"])
+    item = points["meta"]["secs"]
     i = i + 1
 
 sensor_data.close()
 
+print(item)
 print(bots)
+print(i)
+
+# first timestamp 1499093878
+# last timestamp 1499073763
+# ahrs 25 contains 553283 items
